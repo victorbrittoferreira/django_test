0624a62ed4881e3531ffe2d788cb10657b7e914e
Victor de Britto Ferreira <victorbrittoferreira@gmail.com>
2022-01-14 21:42:34 -0300

    models setup

--- myia/models.py
+++ myia/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models import fields
+from django.db.models.fields import DecimalField, Field
+
 
 class Person(models.Model):
     class Meta:
@@ -10,10 +13,7 @@ class Person(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
     phone = models.CharField(max_length=11, null=False, blank=False)
     andress = models.CharField(max_length=255, null=False, blank=False)
-    cpf = models.CharField(max_length=11, null=False, blank=False)
-    
- 
+    cpf = models.CharField(max_length=11, null=False,blank=False, verbose_name='CPF')
 
     def __str__(self):
-        return "#{}".format(self.code)
-
+        return self.name
